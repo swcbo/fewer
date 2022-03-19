103d45e70adf8d58742b0dad5f4f9eb1818dd611
huangbo <1104133609@qq.com>
2022-03-19 21:00:27 +0800

    feat:init

---
+++ packages/app/src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: swcbo
+ * @Date: 2022-03-19 20:55:48
+ * @LastEditors: swcbo
+ * @LastEditTime: 2022-03-19 20:55:50
+ * @FilePath: /fewer/packages/app/src/index.ts
+ * @Description: 入口
+ */
+export default (a: string) => {
+  return a;
+};
